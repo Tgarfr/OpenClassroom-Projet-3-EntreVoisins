f1f316980c2f1b80aa35fe7fe80e4ec5b5478000
Thomas <tgarfr@laposte.net>
2022-10-06 09:57:26 +0200

    Fix a bug to display the list of favorite neighbors after delete a neighbour

--- Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourPagerAdapter.java
+++ Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourPagerAdapter.java
@@ -3,9 +3,15 @@ package com.openclassrooms.entrevoisins.ui.neighbour_list;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.Arrays;
+import java.util.List;
 
 
-public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
+public class ListNeighbourPagerAdapter extends FragmentPagerAdapter implements ViewPager.OnPageChangeListener {
+
+    private final List<Fragment> fragments = Arrays.asList(NeighbourFragment.newInstance(), FavoriteNeighbourFragment.newInstance());
 
     public ListNeighbourPagerAdapter(FragmentManager fm) {
         super(fm);
@@ -18,12 +24,7 @@ public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
      */
     @Override
     public Fragment getItem(int position) {
-        if (position == 1) {
-            return FavoriteNeighbourFragment.newInstance();
-        }
-        else {
-            return NeighbourFragment.newInstance();
-        }
+        return fragments.get(position);
     }
 
     /**
@@ -34,4 +35,16 @@ public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
     public int getCount() {
         return 2;
     }
+
+    @Override
+    public void onPageSelected(int i) {
+        Fragment fragment = fragments.get(i);
+        fragment.onResume(); // FIXME Use FragmentPageAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT instead, but this requires to migrate to Androidx
+    }
+
+    @Override
+    public void onPageScrolled(int i, float v, int i1) {}
+
+    @Override
+    public void onPageScrollStateChanged(int i) {}
 }
